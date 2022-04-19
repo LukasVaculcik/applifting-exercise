c36f97a0095cf4304458451d5c7ffa15311e4a05
Lukáš Vaculčík <lukas.vaculcik@gmail.com>
2022-04-19 14:40:46 +0200

    blank Article component

---
+++ src/component/Article/Article.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Article() {
+  return (
+    <article className="App">
+      
+        
+    </article>
+  );
+}
+
+export default Article;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
 import './index.css';
-import App from './App';
+import App from './component/App';
+import Article from './component/Article';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="article" element={<Article />} />
+            </Routes>
+        </BrowserRouter>,
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
